46c9b354966eadc572402fea7558b50cd415ea82
kura88kura <markosdeveloper@163.com>
2015-05-01 11:59:19 +0000

    devise migrated

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
   # GET /recipes
